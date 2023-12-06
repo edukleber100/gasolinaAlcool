643dae5a4ea0e5658a8e7d782c07e152e5182411
edukleber100 <edukleber100@gmail.com>
2023-12-05 22:48:24 -0300

    Aplicativo de prática finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,28 @@ export class HomePage {
 
   constructor() {}
 
+  public resultado: String = "Resultado"
+  public precoAlcool: number = 0;
+  public precoGasolina: number = 0;
+
+  calcular(){
+
+    //validar campos
+    if(this.precoAlcool && this.precoGasolina){
+      
+      var pAlcool = this.precoAlcool
+      var pGasolina = this.precoGasolina
+
+      var resultado = pAlcool / pGasolina
+
+      if(resultado >= 0.7){
+        this.resultado = "Melhor utilizar Gasolina"
+      } else {
+        this.resultado = "Melhor utilizar Álcool"
+      }
+
+    } else{
+      this.resultado = "Preencha corretamente os campos!"
+    }
+  }
 }
